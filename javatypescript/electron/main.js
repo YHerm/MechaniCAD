93d9d4e76d47176c5bd69e882257dd0b9abcac7c
YHerm <0549405422.yh@gmail.com>
2024-08-30 03:16:25 +0300

    naming, remove preload

--- javatypescript/electron/main.js
+++ javatypescript/electron/main.js
@@ -6,9 +6,10 @@ function createWindow() {
         width: 800,
         height: 600,
         webPreferences: {
-            preload: path.join(__dirname, 'preload.js'),
-            nodeIntegration: true,
-            contextIsolation: false
+            contextIsolation: true, 
+            nodeIntegration: false,
+            enableRemoteModule: false, 
+            sandbox: true
         }
     });
 
